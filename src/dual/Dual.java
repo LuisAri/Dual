426db1e3d8618812c6b9ce627598cd8cdf862345
Hector <hectorlarez1@gmail.com>
2017-01-15 13:11:05 -0430

    Metodo que aplica nueva prueba de valores al tabloide

--- src/dual/Dual.java
+++ src/dual/Dual.java
@@ -23,7 +23,7 @@ public class Dual {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        testTabloide();
+        testTabloide2();
     }
     
     public static void testFuncion(){
@@ -77,6 +77,31 @@ public class Dual {
         System.out.println("\n\n[Pivote]");
         System.out.println(tabla.getPivote());
         
+        tabla.iterar(2);
+    }
+    
+    public static void testTabloide2(){
+        System.out.println("[Funcion Objetivo]");
+        FuncionObjetivo objetivo = new FuncionObjetivo(Caso.MIN, new Float[]{2f, 1f});
+        objetivo.imprimir();
+        System.out.println("\n\n[Restricciones]");
+        
+        Restricciones restricciones = new Restricciones();
+        restricciones.agregarRestriccion(Signo.MAYOR_IGUAL, new float[]{3f,1f}, 3);
+        restricciones.agregarRestriccion(Signo.MAYOR_IGUAL, new float[]{4f, 3f}, 6);
+        restricciones.agregarRestriccion(Signo.IGUAL, new float[]{1f, 2f}, 3);
+        restricciones.imprimir();
+        
+        System.out.println("\n\n[Tabloide]");
+        Tabloide tabla = new Tabloide(objetivo, restricciones);
+        tabla.imprimir();
+        
+        System.out.println("\n\n[Renglon Pivote]");
+        System.out.println(tabla.getRenglonPivote());
+        System.out.println("\n\n[Pivote]");
+        System.out.println(tabla.getPivote());
+        
+        System.out.println("\n\n[Iteracion]");
         tabla.iterar(2);
     }
 }
