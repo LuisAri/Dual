26131e85fd51a081ecbc6aa5681258029c856561
Hector <hectorlarez1@gmail.com>
2017-02-08 04:28:08 -0430

    Creacion de clase con utilidades para el JTable

---
+++ src/dual/tools/TableTools.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dual.tools;
+
+import javax.swing.JTable;
+
+/**
+ *
+ * @author Hector
+ */
+public class TableTools {
+    
+    public static float[] getRowAt(JTable table, int row) {
+        int colNumber = table.getColumnCount();
+        float[] result = new float[colNumber];
+        for (int i = 0; i < colNumber; i++) {
+            result[i] = Float.parseFloat((String)table.getModel().getValueAt(row, i));
+        }
+        return result;
+    }
+}
