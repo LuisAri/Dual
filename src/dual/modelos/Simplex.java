7fcf86eddb1ea433e86ac741d11d41a99b6a7194
Hector <hectorlarez1@gmail.com>
2017-03-15 13:02:58 -0430

    Incorporacion de metodos que iteren la tabla, estados que indiquen el caso especial de la tabla, incorporacion de metodo que imprime la tabla.

--- src/dual/modelos/Simplex.java
+++ src/dual/modelos/Simplex.java
@@ -14,16 +14,26 @@ import java.util.ArrayList;
  */
 public class Simplex {
     
-    private final int RENGLON_OBJETIVO      = 0;
-    private final int RENGLON_RESTRICCION   = 1;
+    private static final int RENGLON_OBJETIVO      = 0;
+    private static final int RENGLON_RESTRICCION   = 1;
+    
+    public static final int TABLA_OPTIMA = 2;
+    public static final int TABLA_MULTIPLES_SOLUCIONES = 3;
+    public static final int TABLA_INFACTIBLE = 4;
+    public static final int TABLA_NO_ACOTADA = 5;
+    public static final int TABLA_SFB = 6;
+    public static final int TABLA_NO_SFB = 7;
     
     private SistemaEcuacion sistema;
 
     private ArrayList<Renglon> renglones;
 
+    private int iteracion;
+    
     public Simplex(SistemaEcuacion sistema){
         this.sistema = sistema;
         renglones = sistema.getEstandar();
+        this.iteracion = 1;
     }
     
     /**
@@ -89,15 +99,90 @@ public class Simplex {
     }
     
     /**
-     * Para escojer la variable de decision que entra, tomaremos el valor de z
-     * con mayor negatividad (ignorando los 0), siendo esa nuestra columna pivote.
+     * Retorna la lista de renglones.
+     * @return 
+     */
+    public ArrayList<Renglon> getRenglones(){
+        return this.renglones;
+    }
+    
+    /**
+     * Reinicia el tabloide a su estado inicial.
+     */
+    public void reiniciar(){
+        renglones = sistema.getEstandar();
+        iteracion = 1;
+    }
+    
+    /**
+     * Permite dirigirse a una iteracion especifica del tabloide si no se 
+     * presenta ningun caso especial o la solucion optima del problema.
      * 
-     * Para encontrar que variable de holgura sale, se tiene que dividir, los 
-     * valores del lado derecho entre su columna pivote, y tomamos la fila con 
-     * el valor de cociente mas pequeño.
-     * 
-     * El elemento pivote se ubica entre la interseccion de la fila y columna 
-     * pivote.
+     * @param iteracion
+     */
+    public void iterar(int iteracion){
+        while(siguiente() == TABLA_SFB && this.iteracion != iteracion){
+            System.out.println("[Simplex.siguiente()] Iteracion #" + this.iteracion);
+        }
+    }
+    
+    /**
+     * Realiza la siguiente iteracion del simplex.
+     */
+    public int siguiente(){
+        // Valido los casos especiales del tabloide.
+        if(this.esOptima()){
+            System.out.println("[Simplex.siguiente()] La tabla es optima, posee una solucion basica factible.");
+            return TABLA_OPTIMA;
+        }else if(this.esNoAcotada()){
+            System.out.println("[Simplex.siguiente()] La tabla es no acotada, posee una columna vnb no positiva.");
+            return TABLA_NO_ACOTADA;
+        }else if(this.esMultipleSoluciones()){
+            System.out.println("[Simplex.siguiente()] La tabla tiene multiples soluciones, una vnb posee valor cero.");
+            return TABLA_MULTIPLES_SOLUCIONES;
+        }else if(this.esInfactible()){
+            System.out.println("[Simplex.siguiente()] La tabla es infactible, no todas las restricciones se satisfacen simultaneamente.");
+            return TABLA_INFACTIBLE;
+        }else if(this.esNoSfb() && iteracion == 1){
+            System.out.println("[Simplex.siguiente()] La tabla no tiene solucion basica factible inicial, se deberia aplicar dual simplex.");
+            return TABLA_NO_SFB;
+        }
+        
+        // Obtengo el valor pivote
+        int r_pivote = this.getFilaPivote();
+        int c_pivote = this.getColumnaPivote();
+        float v_pivote = this.getValor(r_pivote, c_pivote);
+        // Obtengo la division escalar entre el renglon pivote y el valor pivote
+        Renglon division = renglones.get(r_pivote).dividir(v_pivote);
+        // Lo reasigno en la posicion del renglon pivote
+        renglones.set(r_pivote, division);
+        // Recorro cada renglon del tabloide, incluyendo el de la funcion objetivo.
+        for(int j = 0; j < renglones.size(); j++){
+            // Valido si el renglon no es que contiene el pivote.
+            if(j != r_pivote){
+                // De no ser renglon pivote, obtengo el valor de ese renglon con la columna pivote.
+                float v_columna_pivote = this.getValor(j, c_pivote);
+                // Realizo la multiplicacion escalar de la division y el valor negativo.
+                Renglon multiplicacion = division.multiplicar(v_columna_pivote * -1);
+                // Hago la suma entre el renglon actual y el renglon multiplicado.
+                Renglon suma = renglones.get(j).sumar(multiplicacion);
+                // Lo asigno a esa misma posicion.
+                // Si estamos en el renglon objetivo, asignamos true a esZ.
+                if(j == RENGLON_OBJETIVO){
+                    suma.esZ = true;
+                }
+                renglones.set(j, suma);
+            }
+        }
+        
+        // Aumento la iteracion.
+        iteracion++;
+        
+        return TABLA_SFB;
+    }
+    
+    /**
+     * Determina si la sfb actual es no acotada.
      * 
      * Una solucion es no acotada en maximizacion cuando todos los elementos 
      * de una variable no basica negativa, son negativos o cero.
@@ -150,8 +235,16 @@ public class Simplex {
         return false;
     }
     
+    /**
+     * Determina si la sfb es infactible, una sfb es infactible cuando alguna de 
+     * sus restricciones no se puede satisfacer de forma simultanea con el resto
+     * de las demas restricciones, solo se presenta en casos donde haya restricciones
+     * mayor que en el mismo problema lineal.
+     * 
+     * @return 
+     */
     public boolean esInfactible(){
-        return true;
+        return false;
     }
     
     /**
@@ -198,5 +291,33 @@ public class Simplex {
         }
         
         return z.getCoeficientes().todosNegativos();
+    }
+    
+    /**
+     * Determina si existe una sfbi, no existe una sfbi cuando todas las variables
+     * de holgura (VB) son negativas.
+     */
+    public boolean esNoSfb(){
+        return false;
+    }
+    
+    public void imprimir(){
+        int i = 0;
+        for(Renglon renglon: renglones){
+            System.out.print(i++ + " | " + renglon.esZ + " \t| ");
+            
+            int x = 1; 
+            for(Float coeficiente: renglon.coeficientes.coeficientes){
+                System.out.print(coeficiente + "x" + x++ + " \t");
+            }
+            
+            x = 1;
+            for(Float coeficiente: renglon.holguras.coeficientes){
+                System.out.print(coeficiente + "s" + x++ + " \t");
+            }
+            
+            System.out.printf("%.5f", renglon.ladoDerecho);
+            System.out.println("");
+        }
     }
 }
